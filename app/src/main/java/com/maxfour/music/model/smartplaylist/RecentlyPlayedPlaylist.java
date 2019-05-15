62155f850c93424b6c5b4c775530a3b9153c1555
Max <mansurov.maksud@gmail.com>
2019-05-15 11:03:11 +0500

    Update CalendarUtil & Recentlyplayedplaylist, etc.

---
+++ app/src/main/java/com/maxfour/music/model/smartplaylist/RecentlyPlayedPlaylist.java
@@ -0,0 +1,51 @@
+package com.maxfour.music.model.smartplaylist;
+
+import android.content.Context;
+import android.os.Parcel;
+
+import androidx.annotation.NonNull;
+
+import com.maxfour.music.R;
+import com.maxfour.music.loader.TopAndRecentlyPlayedSongsLoader;
+import com.maxfour.music.model.Song;
+import com.maxfour.music.provider.HistoryStore;
+
+import java.util.ArrayList;
+
+public class RecentlyPlayedPlaylist extends AbsSmartPlaylist {
+
+    public RecentlyPlayedPlaylist(@NonNull Context context) {
+        super(context.getString(R.string.recently_played), R.drawable.ic_access_time_white_24dp);
+    }
+
+    @NonNull
+    @Override
+    public ArrayList<Song> getSongs(@NonNull Context context) {
+        return TopAndRecentlyPlayedSongsLoader.getRecentlyPlayedSongs(context);
+    }
+
+    @Override
+    public void clear(@NonNull Context context) {
+        HistoryStore.getInstance(context).clear();
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    protected RecentlyPlayedPlaylist(Parcel in) {
+        super(in);
+    }
+
+    public static final Creator<RecentlyPlayedPlaylist> CREATOR = new Creator<RecentlyPlayedPlaylist>() {
+        public RecentlyPlayedPlaylist createFromParcel(Parcel source) {
+            return new RecentlyPlayedPlaylist(source);
+        }
+
+        public RecentlyPlayedPlaylist[] newArray(int size) {
+            return new RecentlyPlayedPlaylist[size];
+        }
+    };
+}
