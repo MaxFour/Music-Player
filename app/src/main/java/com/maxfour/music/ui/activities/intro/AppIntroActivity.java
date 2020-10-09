9bf749c5f5c7beb1c7a3f24e3c3588af1d84ffeb
Max <mansurov.maksud@gmail.com>
2020-10-09 17:01:48 +0500

    Change AppIntroActivity colors

--- app/src/main/java/com/maxfour/music/ui/activities/intro/AppIntroActivity.java
+++ app/src/main/java/com/maxfour/music/ui/activities/intro/AppIntroActivity.java
@@ -30,16 +30,16 @@ public class AppIntroActivity extends IntroActivity {
                 .title(R.string.label_view_playing_queue)
                 .description(R.string.open_playing_queue_instruction)
                 .image(R.drawable.tutorial_queue_swipe_up)
-                .background(R.color.md_cyan_700)
-                .backgroundDark(R.color.md_cyan_900)
+                .background(R.color.md_teal_500)
+                .backgroundDark(R.color.md_teal_700)
                 .layout(R.layout.fragment_simple_slide_large_image)
                 .build());
         addSlide(new SimpleSlide.Builder()
                 .title(R.string.label_edit_playing_queue)
                 .description(R.string.rearrange_playing_queue_instruction)
                 .image(R.drawable.tutorial_rearrange_queue)
-                .background(R.color.md_deep_orange_700)
-                .backgroundDark(R.color.md_deep_orange_900)
+                .background(R.color.md_indigo_700)
+                .backgroundDark(R.color.md_indigo_900)
                 .layout(R.layout.fragment_simple_slide_large_image)
                 .build());
     }
